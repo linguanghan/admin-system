87501ece91339a46deb26d568c98a7677759cde9
1354009873@qq.com <1354009873@qq.com>
2024-12-01 13:45:19 +0800

    fix：本地跨域修复

--- src/main/java/org/spring/springboot/config/WebConfigurer.java
+++ src/main/java/org/spring/springboot/config/WebConfigurer.java
@@ -28,7 +28,7 @@ public class WebConfigurer implements ServletContextInitializer {
         corsConfiguration.addAllowedOrigin("http://mgrylc.yfqp.shop");
         corsConfiguration.addAllowedOrigin("http://localhost:7456");
         corsConfiguration.addAllowedOrigin("http://127.0.0.1:7456");
-
+        corsConfiguration.addAllowedOrigin("http://localhost:81");
         //  跨域的请求头
         corsConfiguration.addAllowedHeader("*");
         //  跨域的请求方法
